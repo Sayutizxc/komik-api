4cd92c0a7f876327ad8817a5e9ad088be1598846
Muhammad Sayuti <sayutizxc@gmail.com>
2022-01-29 00:17:40 +0800

    Memperbaiki response json yang tidak konsisten

--- model/chapter_images.go
+++ model/chapter_images.go
@@ -1,10 +1,10 @@
 package model
 
 type ChapterImages struct {
-	Chapter  string   `json:"chapter"`
-	Url      string   `json:"url"`
-	Next     string   `json:"next"`
-	Prev     string   `json:"prev"`
-	Images   []string `json:"images"`
-	Comments []Comment
+	Chapter  string    `json:"chapter"`
+	Url      string    `json:"url"`
+	Next     string    `json:"next"`
+	Prev     string    `json:"prev"`
+	Images   []string  `json:"images"`
+	Comments []Comment `json:"comments"`
 }
